52a439f6f065adf3bdd229fe94e90b03cd465db8
keir <hadekshd@gmail.com>
2023-06-02 23:30:05 +0200

    Create date picker

--- app/src/main/java/com/taskstodo/app/AddTaskActivity.kt
+++ app/src/main/java/com/taskstodo/app/AddTaskActivity.kt
@@ -1,11 +1,17 @@
 package com.taskstodo.app
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import java.util.Calendar
 
 class AddTaskActivity : AppCompatActivity() {
+    private lateinit var dateBtn: Button
+    private lateinit var dateText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
@@ -14,6 +20,20 @@ class AddTaskActivity : AppCompatActivity() {
         backButton.setOnClickListener{
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
+        }
+
+        dateBtn = findViewById(R.id.add_task_date_btn)
+        dateText = findViewById(R.id.add_task_date)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        dateBtn.setOnClickListener{
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, myear, mmonth, mday ->
+                dateText.text = "$mday/${mmonth-1}/$myear"
+            }, year, month, day).show()
         }
     }
 }
